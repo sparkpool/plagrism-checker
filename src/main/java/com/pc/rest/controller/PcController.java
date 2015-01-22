904c58c0575aee7666c8de4d11b63a57b636ecd8
Shrey Arora <shrey.arora@paytm.com>
2015-01-22 12:34:22 +0530

    change in code done

--- src/main/java/com/pc/rest/controller/PcController.java
+++ src/main/java/com/pc/rest/controller/PcController.java
@@ -15,36 +15,43 @@ import com.pc.rest.service.intrface.ISearchContentService;
 public class PcController {
 
 	private static final Logger logger = Logger.getLogger(PcController.class);
-	
+
 	private ISearchContentService searchContentService;
-	
+
 	@GET
 	@Path("/content")
 	@Produces("application/json")
-	public String searchContent(@QueryParam("q") String query){
+	public String searchContent(@QueryParam("q") String query) {
 		logger.info("Query received in request is " + query);
 		PlagrismSearchResponse plagrismResponse = null;
-		if(query == null || query.trim().length() == 0 || query.length()>1500){
+		if (query == null || query.trim().length() == 0
+				|| query.length() > 1500) {
 			plagrismResponse = new PlagrismSearchResponse();
-			plagrismResponse.addMessage("Entered Message Must be Non Empty and Length Must be Less than 1500");
+			plagrismResponse
+					.addMessage("Entered Message Must be Non Empty and Length Must be Less than 1500");
 			plagrismResponse.setError("403");
-			return plagrismResponse.toJSON();
-		}
-		plagrismResponse = getSearchService().getSearchResult(query);
-		if(plagrismResponse == null){
-			plagrismResponse = new PlagrismSearchResponse();
-			plagrismResponse.setError("500");
-			plagrismResponse.addMessage("There came an Exception, please try again!!");
-		}else if(plagrismResponse!=null && plagrismResponse.getErrorResponse()!=null){
-			plagrismResponse.addMessage(plagrismResponse.getErrorResponse().getMessage());
-			plagrismResponse.setError(plagrismResponse.getErrorResponse().getCode());
+		} else {
+			plagrismResponse = getSearchService().getSearchResult(query);
+			if (plagrismResponse == null) {
+				plagrismResponse = new PlagrismSearchResponse();
+				plagrismResponse.setError("500");
+				plagrismResponse
+						.addMessage("There came an Exception, please try again!!");
+			} else if (plagrismResponse != null
+					&& plagrismResponse.getErrorResponse() != null) {
+				plagrismResponse.addMessage(plagrismResponse.getErrorResponse()
+						.getMessage());
+				plagrismResponse.setError(plagrismResponse.getErrorResponse()
+						.getCode());
+			}
 		}
 		return plagrismResponse.toJSON();
 	}
-	
-	public ISearchContentService getSearchService(){
-		if(searchContentService == null){
-			searchContentService = (ISearchContentService)InitiateSpringContextListener.getContext().getBean("searchContentService");
+
+	public ISearchContentService getSearchService() {
+		if (searchContentService == null) {
+			searchContentService = (ISearchContentService) InitiateSpringContextListener
+					.getContext().getBean("searchContentService");
 		}
 		return searchContentService;
 	}
